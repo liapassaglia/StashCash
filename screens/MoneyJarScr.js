8d7f5cafba88a6c918ad93c8cd91bfb2f8768bf3
liapassaglia <liapassaglia@gmail.com>
2021-03-21 18:07:55 -0400

    Front-end User Dashboard Scr with rewards and setting modals.

--- screens/MoneyJarScr.js
+++ screens/MoneyJarScr.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Modal} from 'react-native';
+import { Dimensions, StyleSheet, SafeAreaView, ScrollView, Text, View, FlatList, Modal} from 'react-native';
 import { TextInput,Button } from 'react-native-paper';
 import MoneyJar from '../assets/components/MoneyJar';
+
+const {width} = Dimensions.get('window');
+const {height} = Dimensions.get('window');
 
 export default class MoneyJarScr extends React.Component{
   constructor(props){
     super(props);
     this.jarsList = []
     this.state = {
+      screenHeight: 0,
       jars: [],
       inputModalVisible: false,
       label: '',
       goal: '',
     }
   }
-  createJar = (label,goal) => {
+
+  onContentSizeChange = (contentWidth, contentHeight) => {
+    this.setState({screenHeight: contentHeight});
+  }
+
+  createJar = () => {
     const jar = {
       label: this.state.label,
       goal: this.state.goal,
@@ -49,13 +58,22 @@ export default class MoneyJarScr extends React.Component{
   render(){
   const {navigation} = this.props;
   const {jars,inputModalVisible,label,goal} = this.state;
+  const scrollEnabled = this.state.screenHeight > height;
   const renderItem = ({ item }) => {
     return (
       <MoneyJar label={item.label} goal={item.goal}/>
     );
   };
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={{flex: 1}}>
+    <ScrollView 
+      style={{flex:1}}
+      contentContainerStyle={{flexGrow:1}}
+      scrollEnabled={scrollEnabled}
+      onContentSizeChange={this.onContentSizeChange}
+      bounces={false}
+    >
+    <View style={styles.moneyjarView}>    
       <View style={styles.row}>
         <Text style={styles.titleText}>Money Jars</Text>
         <Button
@@ -64,10 +82,10 @@ export default class MoneyJarScr extends React.Component{
           style={styles.createButton}
           onPress={() => this.showModal()}
         >
-          <Text>+ New Jar</Text>
+          <Text style={{color:'#4B674C'}}>+ New Jar</Text>
         </Button>
       </View>
-      <View>
+      <View style={{flexDirection:'row',justifyContent:'center',backgroundColor:'white'}}>
           <FlatList
             data={jars}
             renderItem={renderItem}
@@ -120,19 +138,23 @@ export default class MoneyJarScr extends React.Component{
           </View>
        </Modal>
     </View>
+    </ScrollView>
+    </SafeAreaView>
   );
   }
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  moneyjarView:{
+    flexGrow:1,
+    backgroundColor:'#D0E2D0'
   },
   row:{
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'flex-end',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    backgroundColor: '#4B674C',
+    paddingBottom: 10
   },
   img:{
     width:200,
@@ -142,17 +164,19 @@ const styles = StyleSheet.create({
   },
   titleText:{
     marginTop: 35,
-    marginLeft: 25,
     fontSize: 30,
-    fontWeight: 'bold'
+    marginLeft: 10,
+    color: '#ffffff'
   },
   createButton:{
     height:35,
     width: 125,
-    backgroundColor: '#7AA47A',
+    backgroundColor: '#D0E2D0',
     fontSize: 20,
-    marginTop: 35,
-    marginRight: 25,
+    marginTop: 20,
+    borderColor: '#FFFFFF',
+    borderWidth: 1,
+    marginRight: 10,
   },
   jar:{
     justifyContent: 'center',
