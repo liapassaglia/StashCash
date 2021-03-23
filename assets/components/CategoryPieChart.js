6914282f67b95d43a9b55d1443c6cd0d8facc625
liapassaglia <liapassaglia@gmail.com>
2021-03-23 13:11:03 -0400

    front-end overview screen

---
+++ assets/components/CategoryPieChart.js
@@ -0,0 +1,122 @@
+import React, { Component, useState } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import { PieChart } from 'react-native-chart-kit';
+import { Dimensions } from 'react-native'
+const screenWidth = Dimensions.get('window').width
+
+export default class CategoryPieChart extends Component {
+    constructor(props) {
+        super(props);
+        const {
+            info
+        } = this.props;
+        this.state = {
+            total: 0,
+            inputModalVisible: false,
+            input: 0
+        }
+    }
+    showModal = () => {
+        this.setState({
+            inputModalVisible: true,
+        });
+    }
+    setInput = (text) => {
+        this.setState({
+            input: Number(text).toFixed(2)
+        })
+    }
+    updateTotal = () => {
+        this.setState({
+            total: (Number(this.state.total) + Number(this.state.input)).toFixed(2),
+            inputModalVisible: false
+        });
+    }
+
+
+    render() {
+        const { info } = this.props;
+        const data = [
+            { name: 'Spent', number: info.spent, color: 'rgba(131, 167, 234, 1)', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+            { name: 'Remaining', number: info.remaining, color: '#F00', legendFontColor: '#7F7F7F', legendFontSize: 15 }
+        ]
+        const { total, inputModalVisible, text } = this.state;
+        const chartConfig = {
+            backgroundGradientFrom: '#1E2923',
+            backgroundGradientTo: '#08130D',
+            color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`
+        }
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>{this.props.category}</Text>
+                <Text style={styles.text}>Goal: ${this.props.goal}</Text>
+                <View>
+                    <PieChart
+                        data={data}
+                        width={screenWidth}
+                        height={180}
+                        chartConfig={chartConfig}
+                        accessor="number"
+                        backgroundColor="transparent"
+                        paddingLeft="0"
+                        style={styles.piechart}
+                    />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        flex: 1,
+    },
+    piechart: {
+        flexDirection: row
+    },
+    jar: {
+        backgroundColor: "white",
+        margin: 0,
+    },
+    totalContainer: {
+        alignItems: "center",
+        justifyContent: 'center',
+        marginRight: 50,
+        flex: 1,
+    },
+    labelView: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    labelText: {
+        backgroundColor: 'white',
+        padding: 5,
+        fontSize: 20,
+        borderColor: 'black',
+        borderWidth: 1
+    },
+    text: {
+        fontSize: 20,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    }
+});
--- screens/OverviewScr.js
+++ screens/OverviewScr.js
@@ -1,21 +1,437 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import { SafeAreaView, Dimensions, ScrollView, StyleSheet, Text, View, Image, Modal} from 'react-native';
+import { List, Button, Switch, Card, Title, Paragraph, IconButton, TextInput, ToggleButton } from 'react-native-paper';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 
-export default function OverviewScr({navigation}) {
+const {width} = Dimensions.get('window');
+const {height} = Dimensions.get('window');
+const allData = [
+  { name: 'Gas', alotted: 80, spent: 50, remaining: 30, color: 'rgba(131, 167, 234, 1)', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+  { name: 'Groceries', alotted: 200, spent: 120, remaining: 80, population: 2800000, color: '#000', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+  { name: 'Clothes', alotted: 80, spent: 10, remaining: 70, population: 527612, color: 'red', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+  { name: 'Venmo', alotted: 35, spent: 5, remaining: 30, population: 8538000, color: '#ffffff', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+  { name: 'Other', alotted: 85, spent: 22, remaining: 63, population: 11920000, color: 'rgb(0, 0, 255)', legendFontColor: '#7F7F7F', legendFontSize: 15 }
+]
+
+export default class DashboardScr extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.userData = [];
+    this.dataOverview = [
+      {
+        name: "Coffee",
+        population: 6,
+        color: "#46A84A",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+      },
+      {
+        name: "Going Out",
+        population: 30,
+        color: "#74D795",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+      },
+      {
+        name: "Gas",
+        population: 15,
+        color: "#24C545",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+      },
+      {
+        name: "Eating Out",
+        population: 10,
+        color: "#819C71",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+      },
+      {
+        name: "Remaining",
+        population: 34,
+        color: "#D0D0D0",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+      },
+    ];
+    this.dataCategories = [
+      {
+        category: 'Coffee',
+        remaining: 4,
+        spent: 6,
+        alloted: 10
+      },
+      {
+        category: 'Going Out',
+        remaining: 0,
+        spent: 30,
+        alloted: 30
+      },
+      {
+        category: 'Gas',
+        remaining: 20,
+        spent: 15,
+        alloted: 35
+      },
+      {
+        category: 'Eating Out',
+        remaining: 10,
+        spent: 25,
+        alloted: 40
+      }
+    ]
+    this.state = {
+        screenHeight: 0,
+        currentIndex: 0,
+        data: [],
+    }
+    //preserve inital states
+    this.baseState = this.state
+  }
+
+  onContentSizeChange = (contentWidth, contentHeight) => {
+    this.setState({screenHeight: contentHeight});
+  }
+
+  onCancel = () => {
+    this.setState(this.baseState)
+  }
+
+  componentDidMount() {
+    this.setState({data: [...this.userData]})
+  }
+
+  setValue(value) {
+    this.setState({value: value});
+  }
+
+  render(){
+  const {data,value} = this.state;
+  const scrollEnabled = this.state.screenHeight > height;
+ 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Overview</Text>
-      <StatusBar style="auto" />
+    <ScrollView 
+      style={{flex:1}}
+      contentContainerStyle={{flexGrow:1}}
+      scrollEnabled={true}
+      onContentSizeChange={this.onContentSizeChange}
+      bounces={false}
+    >
+      <View style={styles.dashboardView}>
+        <View style={styles.row}>
+          <Text style={styles.titleText}>Spending Overview</Text>
+          <IconButton
+            icon="cash-plus"
+            color={'#ffffff'}
+            size={40}
+            style={{marginTop: 10}}
+          />
+        </View>
+        <View style={styles.weeklyOverview}>
+          <Text style={{alignSelf:'center',fontSize:25,margin:10}}>Weekly Status</Text>
+          <PieChart
+            data={this.dataOverview}
+            width={Dimensions.get('window').width - 20}
+            height={220}
+            chartConfig={{
+              backgroundColor: '#F9F9F9',
+              backgroundGradientFrom: '#F9F9F9',
+              backgroundGradientTo: '#F9F9F9',
+              decimalPlaces: 2,
+              color: (opacity = 1) => `rgba(70, 168, 74, ${opacity})`,
+              style: {
+                borderRadius: 10,
+              },
+            }}
+            accessor={"population"}
+            style={{
+              marginVertical: 8,
+              borderRadius: 16,
+              alignSelf:'center',
+              backgroundColor: '#F9F9F9'
+            }}
+          />
+        </View>
+        <Text style={{alignSelf:'center',fontSize:25,margin:10}}>Categories</Text>
+        <View style={styles.category}>
+
+
+        <View style={{width, height:200, marginLeft:75}}>
+                  <ScrollView
+                    pagingEnabled horizontal style={styles.category} centerContent
+                  >
+                    {
+                      dataCategories.map((image,index)=>(
+                        <Card style={styles.cardModal}>
+                          <Card.Content style={styles.cardContent}>
+                            <View style={{width:'60%'}}>
+                              <Title style={{fontSize:25}}>{image.name}</Title>
+                              <Paragraph style={{fontSize:17}}>{image.description}</Paragraph> 
+                            </View>
+                            <View style={styles.cardImageView}>
+                            {points >= image.points ?
+                              <Button
+                                mode="contained"
+                                uppercase={false}
+                                style={styles.cardButtonModal}
+                                onPress={() => this.claimReward(image)}
+                              >
+                                <Text>Claim</Text>
+                              </Button>
+                              :
+                              <Button
+                                mode="contained"
+                                uppercase={false}
+                                style={[styles.cardButtonModal,{backgroundColor: '#F9F9F9'}]}
+                              >
+                              </Button>
+                              }
+                              <Image
+                                style={{
+                                  alignSelf: 'center',
+                                  height: 50,
+                                  width: 50,
+                                  borderWidth: 1,
+                                  borderRadius: 75
+                                }}
+                                source={{uri:image.url}}
+                                resizeMode="stretch"
+                              />
+                              </View>
+                          </Card.Content>
+                          <Paragraph style={{fontSize:20, marginTop: 10, marginLeft: 20}}>Points Needed:</Paragraph> 
+                          <Paragraph style={{fontSize:20, marginTop: 10, marginLeft: 20, fontWeight:'bold'}}>{image.points}</Paragraph>
+                        </Card>
+                      ))
+                    }
+                  </ScrollView>
+              </View>
+
+
+          <Text style={{alignSelf:'center',fontSize:25,marginTop:20}}>Coffee</Text>
+          <View style={{flexDirection:'row'}}>
+          <ProgressChart
+            data={{
+              labels: ["Remaining"], 
+              data: [0.6]}
+            }
+            width={Dimensions.get('window').width/2}
+            height={125}
+            strokeWidth={16}
+            radius={30}
+            chartConfig={{
+              backgroundColor: '#F9F9F9',
+              backgroundGradientFrom: '#F9F9F9',
+              backgroundGradientTo: '#F9F9F9',
+              decimalPlaces: 2,
+              color: (opacity = 1) => `rgba(70, 168, 74, ${opacity})`,
+            }}
+            hideLegend={true}
+            accessor={"population"}
+            style={{
+              borderRadius: 16,
+              alignSelf:'center',
+            }}
+          />
+          <View style={{marginTop:20,marginLeft:-20}}>
+            <Text style={{fontSize:17,margin:5,fontWeight:'bold',alignSelf:'center'}}>
+              Remaining: $4
+            </Text>
+            <Text style={{fontSize:17,margin:5,alignSelf:'center'}}>
+              Total Spent: $6
+            </Text>
+            <Text style={{fontSize:17,margin:5,alignSelf:'center'}}>
+              Budget Alloted: $10
+            </Text>
+          </View>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
     </SafeAreaView>
   );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  dashboardView:{
+    flexGrow:1,
+    backgroundColor:'#D0E2D0'
+  },
+  row:{
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-end',
+    justifyContent: 'space-between',
+    backgroundColor: '#4B674C',
+    paddingBottom: 10
+  },
+  weekyOverview:{
+    padding: 20,
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  titleText:{
+    marginTop: 35,
+    fontSize: 30,
+    marginLeft: 10,
+    color: '#ffffff'
+  },
+  settingsButton:{
+    height:35,
+    width: 125,
+    backgroundColor: '#D0E2D0',
+    fontSize: 20,
+    marginTop: 20,
+    borderColor: '#FFFFFF',
+    borderWidth: 1,
+    marginRight: 10,
+  },
+  streak:{
+    padding: 20,
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  streakText:{
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  points:{
+    marginLeft: 25,
+    marginRight: 25,
+    padding: 20,
+    justifyContent: 'center',
+    backgroundColor: '#F9F9F9'
+  },
+  pointText:{
+    alignSelf: 'center',
+    fontSize: 20,
+  },
+  pointNumber:{
+    alignSelf: 'center',
+    fontSize: 30,
+    marginTop: 10
+  },
+  userRewardsButton:{
+    height:35,
+    width: 200,
+    backgroundColor: '#9FB48F',
+    fontSize: 20,
+    alignSelf: 'center',
+    marginTop:20,
+    marginBottom:20,
+  },
+  userRewards:{
+    padding: 20,
     justifyContent: 'center',
   },
+  userRewardsText:{
+    marginLeft:16,
+    fontSize: 17,
+  },
+  card:{
+    height: 150,
+    width: 355,
+    backgroundColor: '#F9F9F9'
+  },
+  cardContent:{
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  cardImageView:{
+    alignSelf: 'flex-end',
+  },
+  cardButton:{
+    height:35,
+    width: 125,
+    backgroundColor: '#9FB48F',
+    fontSize: 20,
+    marginLeft:20,
+    marginBottom:20,
+  },
+  swiperTitle:{
+    fontSize: 20,
+    marginBottom: 10,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: '#7AA47A',
+    borderRadius: 20,
+    height: '60%',
+    width: '90%',
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  modalText: {
+    padding: 15,
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    textAlign: "center"
+  },
+  modalButton:{
+    height:35,
+    width: 200,
+    backgroundColor: '#ffffff',
+    margin: 30,
+    fontSize: 20
+  },
+  modalButtonText: {
+      color: '#5B5B5B',
+      fontSize: 15,
+      textAlign: "center"
+  },
+  cardModal:{
+    height: 600,
+    width: 300,
+    backgroundColor: '#F9F9F9'
+  },
+  cardButtonModal:{
+    height:35,
+    width: 75,
+    backgroundColor: '#9FB48F',
+    fontSize: 20,
+    marginLeft:20,
+    marginBottom:20,
+  },
+  inputBox:{
+    height: 50,
+    width: 250,
+    backgroundColor: '#ffffff',
+    marginBottom: 25,
+    alignSelf: 'center',
+    fontSize: 20,
+  },
+  category:{
+    backgroundColor: '#F9F9F9',
+    width: width - 20,
+    alignSelf: 'center',
+  }
 });
